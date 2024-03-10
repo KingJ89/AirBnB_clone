5c50b55f06fcd184ac1d3db0b1c3a7adb476b76d
KingJ89 <janmutewera@gmail.com>
2024-03-10 15:48:42 -0700

    Implement AirBnB console and unit tests

--- tests/test_Hbnbconsole.py
+++ tests/test_Hbnbconsole.py
@@ -1,63 +1,66 @@
 #this is the main the main test file for the console project By Jan Yaya Mutewera
 
-import unittest
-from unittest.mock import patch
+#!/usr/bin/python3
+""" Test Console for AirBnB """
+import cmd
+import sys
 from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
 from console import HBNBCommand
 
-class TestHBNBCommand(unittest.TestCase):
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_EOF(self, mock_stdout):
-        console = HBNBcommand()
-        self.assertTrue(console.do_EOF(''))
+class TestConsole(TestCase):
+    """ Test class for the console """
 
+    def setUp(self):
+        """ Redirect stdout for testing"""
+        self.console_out = StringIO()
+        sys.stdout = self.console_out
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_quit(self, mock_stdout):
-        console = HBNBCommand()
-        self.assertTrue(console.do_quit(''))
+    def tearDown(self):
+        """ Reset redirect """
+        sys.stdout = sys.__stdout__
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_emptyline(self, mock_stdout):
-        console = HBNBCommand()
-        console.emptyline()
-        self.assertEqual(mock_stdout.getvalue(), '')
+    def test_create(self):
+        """ Test create command """
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            HBNBCommand().onecmd("create BaseModel")
+            self.assertTrue(len(fake_out.getvalue()) > 0)
 
-    @patch('sys.stdout' new_callable=StringIO)
-    def test_do_create(self, mock_stdout):
-        console = HBNBCommand()
-        with patch('sys.stdin', StringIO('BaseModel\nexit\n')):
-            console.cmdloop()
-        self.assertTrue(mock_stdout.getvalue().startswith('(hbnb) '))
+    def test_show(self):
+        """ Test show command """
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            HBNBCommand().onecmd("show BaseModel 1234-1234-1234")
+            self.assertEqual(fake_out.getvalue(), "** no instance found **\n")
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_clear(self, mock_stdout):
-        console = HBNBCommand()
-        with patch('sys.stdin', StringIO('BaseModel\nexit\n')):
-            console = cmdloop()
-        self.assertTrue(mock_stdout.getvalue().startswith('(hbnb) '))
+    def test_destroy(self):
+        """ Test destroy command """
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            HBNBCommand().onecmd("destroy BaseModel 1234-1234-1234")
+            self.assertEqual(fake_out.getvalue(), "** no instance found **\n")
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_show(self, mock_stdout):
-        console = HNBNCommand()
-        with patch('sys.stdin', StringIO('BaseModel\nextit\n')):
-            console.cmdloop()
-        self.assertTrue(mock_stdout.getvalue().startswith('(hbnb) '))
+    def test_all(self):
+        """ Test all command """
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            HBNBCommand().onecmd("all")
+            self.assertTrue(len(fake_out.getvalue()) > 0)
 
-    @patc('sys.stdout', new_callable=StringIO)
-    def test_do_destroy(self, mock_stdout):
-        console = HBNBCommand()
-        with patch('sys.stdin', StringIO('BaseModel\nexit\n')):
-            console.cmdloop()
-        self.assertTrue(mock_stdout.getvalue().startswith('(hbnb) '))
+    def test_update(self):
+        """ Test update command """
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            HBNBCommand().onecmd("update BaseModel 1234-1234-1234")
+            self.assertEqual(fake_out.getvalue(), "** no instance found **\n")
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_all(self, mock_stdout):
-        console = HBNBCommand()
-        with patch('sys.stdin', StringIO('BaseModel\nexit\n')):
-            console.cmdloop()
-        self.assertTrue(mock_stdout.getvalue().startswith('(hbnb) '))
+    def test_quit(self):
+        """ Test quit command """
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            HBNBCommand().onecmd("quit")
+            self.assertEqual(fake_out.getvalue(), "")
 
-        if __name__ '__main__':
-            unittest.main()
+    def test_EOF(self):
+        """ Test EOF command """
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            HBNBCommand().onecmd("EOF")
+            self.assertEqual(fake_out.getvalue(), "")
+
