4a754949da39e7ad81a28c0144af41a1227d7945
KingJ89 <janmutewera@gmail.com>
2024-05-19 19:31:10 -0700

    Task city, amenity, basemodel completed successfully

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,44 @@
 #!/usr/bin/python3
-"""Module for the Amenity class."""
+"""Functional test cases for the BaseModel class."""
 
-from models.base_model import BaseModel
+import uuid
+from datetime import datetime
+import models
 
+class BaseModel:
+    """
+    BaseModel class defines common attributes/methods for other classes
+    """
 
-class Amenity(BaseModel):
-    """Amenity class inherits from BaseModel."""
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialization of BaseModel Class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k in ["created_at", "updated_at"]:
+                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif k != "__class__":
+                    setattr(self, k, v)
+        else:
+            models.storage.new(self)
 
-    def __init__(self, *args, **kwargs):
-        """Initialization of Amenity instance."""
-        super().__init__(*args, **kwargs)
-        self.name = ""
+    def __str__(self) -> str:
+        """Returns the string representation of an instance"""
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def save(self) -> None:
+        """Update the public instance updated_at"""
+        self.updated_at = datetime.utcnow()
+        models.storage.save()
+
+    def to_dict(self) -> dict:
+        """Returns the dictionary representation of the instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        for k, v in obj_dict.items():
+            if isinstance(v, datetime):
+                obj_dict[k] = v.isoformat()
+        return obj_dict
