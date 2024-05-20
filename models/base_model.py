01c8f70ac0632b991e08f934fa6efff197074faf
KingJ89 <janmutewera@gmail.com>
2024-05-19 19:17:02 -0700

    models.py complete

---
+++ models/base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for the Amenity class."""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class inherits from BaseModel."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of Amenity instance."""
+        super().__init__(*args, **kwargs)
+        self.name = ""
